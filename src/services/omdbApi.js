f34d8d3baf868014021d57dd5071e59a163ef42f
daps <ydapo50@gmail.com>
2025-01-15 11:42:03 +0100

    add axios dependency and implement OMDb API service functions

---
+++ src/services/omdbApi.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+/**
+ * Base URL for the Open Movie Database (OMDb) API
+ * Note: This API requires an API key for authentication
+ * @constant {string} BASE_URL
+ * @see {@link http://www.omdb.com/} OMDb API Documentation
+ */
+const BASE_URL = `http://www.omdb.com/`;
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+export const fetchAllMovies = async (page = 1) => {
+  try {
+    const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&i=${page}`);
+    return response.data;
+  } catch (error) {
+    console.log({ Error: error.message });
+  }
+};
+
+export const fetcMovieDetailsById = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&i=${id}`);
+    return response.data;
+  } catch (error) {
+    console.log({ Error: error.message });
+    return null;
+  }
+};
+
+export const fetchMovieByTitle = async (title) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?apikey=${API_KEY}*T=${title} `
+    );
+    return response.data;
+  } catch (error) {
+    console.log({ Error: error.message });
+    return null;
+  }
+};
