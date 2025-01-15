8eb04db915af125cc5af7fe67d6016cf0758a695
daps <ydapo50@gmail.com>
2025-01-15 14:27:42 +0100

    update TrendingMovieCard component to display movie poster and title

--- src/components/TrendingMovieCard.jsx
+++ src/components/TrendingMovieCard.jsx
@@ -1,5 +1,15 @@
-function TrendingMovieCard() {
-  return <div>TrendingMovieCard</div>;
+/* eslint-disable react/prop-types */
+function TrendingMovieCard({ movie }) {
+  return (
+    <div className="bg-red-600 w-[65rem] text-[#e2e2e2] rounded-xl p-5">
+      <img
+        src={movie.Poster}
+        className="w-full object-cover h-[30rem]"
+        alt=""
+      />
+      {movie?.Title}
+    </div>
+  );
 }
 
 export default TrendingMovieCard;
