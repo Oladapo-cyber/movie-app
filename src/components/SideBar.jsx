724b545d24b1cc8d96ee7da0c7e1c4b4ae2e786e
daps <ydapo50@gmail.com>
2025-01-20 15:03:07 +0100

    fix: update SideBar component to be fixed and adjust width for improved layout

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,9 @@
 function SideBar() {
-  return <div className="w-[40rem] bg-slate-900 h-screen">SideBar</div>;
+  return (
+    <div className="w-[15rem] fixed z-20 mt-20 p-5 bg-slate-900 h-screen">
+      SideBar
+    </div>
+  );
 }
 
 export default SideBar;
