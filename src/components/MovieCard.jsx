e31b5e1ec68fa69ccf1adc6a8d99c6b42ca11d53
daps <ydapo50@gmail.com>
2025-01-21 12:38:45 +0100

    feat: enhance MovieCard component to display movie details with improved layout

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,18 @@
-function MovieCard() {
-  return <div>MovieCard</div>;
-}
+/* eslint-disable react/prop-types */
+
+const MovieCard = ({ movie }) => {
+  console.log(movie);
+  return (
+    <div className="flex flex-col gap-4 hover:cursor-pointer bg-slate-100/10 p-5 rounded-lg">
+      <img src={``} className="h-64 rounded-xl" alt="" />
+      <div className="flex flex-col text-sm">
+        <h2 className=" font-bold">{movie.title}</h2>
+        <span className="text-xs text-slate-200">
+          rating | {movie.release_date}
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default MovieCard;
