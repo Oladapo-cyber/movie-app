a8aa412a402d80d145ebbdc6c5cd6ec137540cc5
daps <ydapo50@gmail.com>
2025-01-27 12:45:44 +0100

    feat: refactor MovieDetails component to use MovieDetailsCard for improved structure

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,14 +2,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { options } from "../services/omdbApi";
-import {
-  FaPlay,
-  FaPlus,
-  FaThumbsDown,
-  FaThumbsUp,
-  FaVideo,
-} from "react-icons/fa";
+
 import Recommendations from "../components/Recommendations";
+import MovieDetailsCard from "../components/MovieDetailsCard";
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState(null);
@@ -41,85 +36,7 @@ const MovieDetails = () => {
         alt=""
         className=""
       />
-      <div className="absolute flex z-10 bg-slate-200 text-black h-[27rem] w-[90%]  mt-[37rem] rounded-2xl p-4 pt-6">
-        <div className="flex flex-col ">
-          <img
-            src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
-            alt=""
-            className="h-[15rem] object-cover h rounded-xl"
-          />
-
-          <p className="pt-2">
-            <span className="text-xl text-[#1baf69] font-semibold">
-              {movie?.vote_average}
-            </span>
-          </p>
-          <div className="py-[1.5px] px-15 bg-[#1baf69] "></div>
-          <div className="flex mt-2 gap-3">
-            <button className="flex items-center gap-1 font-medium text-base text-white bg-[#1baf69] px-4 rounded-sm">
-              <FaThumbsUp /> Like
-            </button>
-            <button className="flex items-center gap-1 font-medium text-base bg-[#444444] text-white px-2 rounded-sm">
-              <FaThumbsDown /> Dislike
-            </button>
-          </div>
-        </div>
-        <div className="px-6">
-          <div className="flex justify-between">
-            <button className="flex items-center gap-1 font-base text-lg bg-[#efc121] px-3 py-1 rounded-3xl">
-              <FaPlay /> Watch now
-            </button>
-
-            <button className="flex items-center gap-1 font-base text-lg bg-[#eeeeee] px-3 py-1 rounded-3xl">
-              <FaPlus /> Add to favorites
-            </button>
-          </div>
-          <p className=" text-4xl pt-3">{movie?.title}</p>
-          <div className="flex gap-3 mt-2">
-            <button className="flex items-center rounded-md justify-center border-2 border-gray-500 p-1 gap-2">
-              <FaVideo /> Trailer
-            </button>
-            <button className="font-bold rounded-md border-2 border-gray-500 p-1 text-lg">
-              HD
-            </button>
-          </div>
-          <p className="pt-3">{movie?.overview}</p>
-
-          <div className="flex justify-between pr-10">
-            <ul className="mt-3">
-              <li>
-                <span className="text-base font-semibold">Released: </span>
-                {movie?.release_date}
-              </li>
-              <li className="mt-1">
-                <span className="text-base font-semibold">Genre: </span>
-                {movie?.genres.map((genre) => (
-                  <span key={genre.id}>{genre.name},</span>
-                ))}
-              </li>
-            </ul>
-
-            <ul className="mt-3 pr-32">
-              <li>
-                <span className="text-base font-semibold">Runtime: </span>
-                {movie?.runtime} Mins
-              </li>
-              <li className="mt-1">
-                <span className="text-base font-semibold">Country: </span>
-                {movie?.production_countries.map((country) => (
-                  <span key={country.id}>{country.name},</span>
-                ))}
-              </li>
-              <li className="mt-1">
-                <span className="text-base font-semibold">Production: </span>
-                {movie?.production_companies.map((company) => (
-                  <span key={company.id}>{company.name},</span>
-                ))}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <MovieDetailsCard movie={movie} />
       <Recommendations id={id} tvPath={tvPath} />
     </div>
   );
